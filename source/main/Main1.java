a9626c4e8421d411dad2d939b8833b8406e93e6c
João Dias <joaovitorelf@gmail.com>
2023-03-20 23:12:44 -0300

    Exercício QuickSort com Libs

---
+++ source/main/Main1.java
@@ -0,0 +1,18 @@
+package main;
+
+import controller.Controller;
+import view.Main;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		
+		Controller controller = new Controller();
+		Main main = new Main();
+		
+		// controller.
+		
+
+	}
+
+}
